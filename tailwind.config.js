4ed6bdf2d40f87a597a6ca438fe8e9056692b26e
AdnanAmin045 <adnanamin.available@gmail.com>
2024-08-08 11:12:47 -0700

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   theme: {
     screens: {
       'mobile': '300px',
