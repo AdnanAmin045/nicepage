6725803e347c17ddd3e01b8f4fa932a3cfd0f228
AdnanAmin045 <adnanamin.available@gmail.com>
2024-08-08 03:01:32 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'mobile': '300px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '600px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1000px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(90deg, rgb(54, 107, 209, 1) 0%, rgb(171, 62, 141, 1) 100%)',
+        'custom-gradient2': 'linear-gradient(90deg, rgb(171, 62, 141, 1) 0%, rgb(54, 107, 209, 1) 100%)',
+      },
+    },
+  },
+  plugins: [],
+}
